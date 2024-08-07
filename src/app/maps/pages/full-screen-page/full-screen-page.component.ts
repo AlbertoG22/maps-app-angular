f7ee0384a6c0441ae4bcf75b791e846b13081521
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-08-07 17:25:28 -0600

    ViewChild - ElementRef

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
-
-(mapboxgl as any).accessToken = 'pk.eyJ1IjoiYmV0b2dtIiwiYSI6ImNsemthMnh5YTB5cDAya3EyOG53ZHg4aGcifQ.p3uF8vPAa8LlgRXRfYQi5A';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'
 
 @Component({
   templateUrl: './full-screen-page.component.html',
@@ -9,9 +7,13 @@ import mapboxgl from 'mapbox-gl';
 })
 export class FullScreenPageComponent implements AfterViewInit {
   
+  @ViewChild('map') divMap?: ElementRef;
+  
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+    if( !this.divMap ) throw 'El elemento HTML no fue encontrado.';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom

1f33719e2b1f27688affac1d1597c3b2daf93ebb
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-08-08 15:41:17 -0600

    add map and marker activity

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: ``
+  styleUrls: ['./mini-map.component.css'],
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  public map?: Map;
+  
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+  
+  ngAfterViewInit(): void {
+    if( !this.divMap?.nativeElement ) throw "Map Div not found";
+    if( !this.lngLat ) throw "LngLat can't be null";
 
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+    });
+
+    this.addMarker();
+  }
+
+  addMarker() {
+    if( !this.map ) return;
+
+    const marker = new Marker({
+      color: 'red',
+      draggable: false
+    })
+      .setLngLat( this.lngLat! )
+      .addTo( this.map );
+  }
+  
 }

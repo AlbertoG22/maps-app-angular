8105505f2876078eb09de87b756e9e4c444363a8
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-08-09 15:16:26 -0600

    standalone as a reusable component

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
 
 @Component({
   standalone: true,
+  imports: [ CounterAloneComponent ],
   templateUrl: './alone-page.component.html',
-  styleUrl: './alone-page.component.css'
+  styleUrl: './alone-page.component.css',
 })
 export class AlonePageComponent {
 
